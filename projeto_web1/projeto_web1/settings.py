bae117def6176c9299e5643b39319b1513f9e98f
João Vitor Lima Castro <joaovitorlimacastroteixeira@gmail.com>
2023-06-14 13:42:01 -0300

    Configuração dos templates e linguagem do projeto

--- projeto_web1/projeto_web1/settings.py
+++ projeto_web1/projeto_web1/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'projeto_web1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"], #direciona pra pasta de templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
