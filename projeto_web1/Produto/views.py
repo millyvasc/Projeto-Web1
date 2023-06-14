444e0504accc08f95e57e90b4b18926a74200867
John <fera55@live.com>
2023-06-14 14:59:28 -0300

    produtos

--- projeto_web1/Produto/views.py
+++ projeto_web1/Produto/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render  # IMPORTA
 from django.template import loader
 from Produto.forms import ProdutoForm  # IMPORTA
-from Produto.models import Pessoas
+from Produto.models import Produtos
 
 
 
