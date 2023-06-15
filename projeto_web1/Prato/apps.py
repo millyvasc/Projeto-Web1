b5c339442096d65df1a291f8871bcee94a1311e6
João Vitor Lima Castro <joaovitorlimacastroteixeira@gmail.com>
2023-06-15 12:51:46 -0300

    Deleção do app Prato

--- projeto_web1/Prato/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PratoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Prato'
--- projeto_web1/Produto/models.py
+++ projeto_web1/Produto/models.py
@@ -4,6 +4,7 @@ class Produtos(models.Model):
     cod = models.CharField(max_length=20)
     nome = models.CharField(max_length=100)
     valorUnitario = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.CharField(max_length=200)
     estoque = models.IntegerField()
 
     def __str__(self):
