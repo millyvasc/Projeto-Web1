57faee3fe289d6021588c92a47655d8181af357d
João Vitor Lima Castro <joaovitorlimacastroteixeira@gmail.com>
2023-06-15 13:09:24 -0300

    Criação model de Pedido

--- projeto_web1/Pedido/models.py
+++ projeto_web1/Pedido/models.py
@@ -1,3 +1,11 @@
+from projeto_web1.Produto.models import Produto
 from django.db import models
 
-# Create your models here.
+class Pedido(models.Model):
+    cod = models.BigAutoField(max_length=20, primary_key=True) #BigAutoField, pra ser auto incrementável
+    produtos = models.ManyToManyField(Produto, related_name="Produtos")#Muitos produtos, podem estar em muitos pedidos
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    observacao = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
--- projeto_web1/projeto_web1/settings.py
+++ projeto_web1/projeto_web1/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'Comanda',
     'Pedido',
     'Produto',
-    'Prato',
     'Funcionario',
     'Administrador',
     'Garcom',
