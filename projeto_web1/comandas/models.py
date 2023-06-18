0a82b06cd64c52cbe8a4ed74cfdefb54de75aa5e
João Vitor Nascimento <96423586+joaovitornso@users.noreply.github.com>
2023-06-18 10:27:06 -0300

    .

--- projeto_web1/comandas/models.py
+++ projeto_web1/comandas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Comanda(models.Model):
     cod = models.BigAutoField(primary_key=True) #BigAutoField, pra ser auto incrementável
     valorTotal = models.DecimalField(max_digits=10, decimal_places=2)
-    mesa = models.IntegerField()
+    num_mesa = models.IntegerField()
 
     def __str__(self):
         return "Comanda "+str(self.cod) #Pra tentar mandar uma mensagem bonitinha tipo 'Comanda = 103'
