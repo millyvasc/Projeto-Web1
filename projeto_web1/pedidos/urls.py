d154884b251b7cd68bd5da8ff4ba81d2548d1d9a
João Vitor Lima Castro <joaovitorlimacastroteixeira@gmail.com>
2023-06-21 17:44:44 -0300

    Remoção de produtos do carrinho

--- projeto_web1/pedidos/urls.py
+++ projeto_web1/pedidos/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("adicionar/<int:mesa1>/<int:cod_produto>/", views.adicionar, name="adicionar"),
+    path("<int:mesa1>/adicionar/<int:cod_produto>/", views.adicionar, name="adicionar"),
     path("<int:mesa1>/carrinho/", views.list_carrinho, name="list_carrinho"),
+    path("<int:mesa1>/carrinhoRemover/<int:cod_produto>/", views.remover_carrinho, name="remover_carrinho"),
+    path("<int:mesa1>/carrinhoRemoverConfirmar/<int:cod_produto>/", views.remover_carrinho_confirmar, name="remover_carrinho_confirmar"),
     #path("<int:produto_cod>/", views.verProduto, name="detalhes"),
 ]
--- projeto_web1/pedidos/views.py
+++ projeto_web1/pedidos/views.py
@@ -98,15 +98,76 @@ def list_carrinho(request, mesa1):
             for i in produtosPedidos:
                 for a in dsPratosAux:
                     if i.cod_produto.cod==a.cod:
+                        i.cod_produto.estoque=i.quantidade
                         dsPratos.append(i.cod_produto)
             #busco as bebidas
             for i in produtosPedidos:
                 for a in dsBebidasAux:
                     if i.cod_produto.cod==a.cod:
-                        print(str(i.quantidade))
+                        i.cod_produto.estoque=i.quantidade
                         dsBebidas.append(i.cod_produto)
             
             
             contexto = {'mesa': mesa1, 'dsPratos': dsPratos, 'dsBebidas': dsBebidas}
             
             return render(request, "pedidos/carrinho.html", contexto)
+        
+def remover_carrinho(request, mesa1, cod_produto):
+    dsComanda = Comanda.objects.filter(status=0, mesa=mesa1) 
+    if dsComanda.count()== 0: #verifico se não há comanda
+        return render(request, "pedidos/carrinhoVazio.html") #se não tiver
+    else:
+        for i in dsComanda: #se tiver busco
+            if i.status==0:
+                comanda = Comanda.objects.get(pk=i.cod)
+        
+        dsPedido = Pedido.objects.filter(status=0, comanda=comanda.cod)
+        if dsPedido.count()==0: #verifico se há pedidp
+            return render(request, "pedidos/carrinhoVazio.html")
+        else: #se houver, busca ele
+            for i in dsPedido:
+                if i.status==0:
+                    pedido = Pedido.objects.get(pk=i.cod)
+                    
+    vProduto = Produto.objects.get(pk=cod_produto)
+    produto=Pedido_Produto.objects.filter(cod_pedido=pedido.cod, cod_produto=vProduto.cod)
+    for i in produto:
+        if i.cod_produto.cod==vProduto.cod:
+            vProduto.estoque=i.quantidade
+    
+    contexto = {'mesa': mesa1, 'vProduto': vProduto}
+            
+    return render(request, "pedidos/carrinhoRemover.html", contexto)
+
+def remover_carrinho_confirmar(request, mesa1, cod_produto):
+    quantidadeDeletar = int(request.POST.get('quantidade'))
+    dsComanda = Comanda.objects.filter(status=0, mesa=mesa1) 
+    if dsComanda.count()== 0: #verifico se não há comanda
+        return render(request, "pedidos/carrinhoVazio.html") #se não tiver
+    else:
+        for i in dsComanda: #se tiver busco
+            if i.status==0:
+                comanda = Comanda.objects.get(pk=i.cod)
+        
+        dsPedido = Pedido.objects.filter(status=0, comanda=comanda.cod)
+        if dsPedido.count()==0: #verifico se há pedidp
+            return render(request, "pedidos/carrinhoVazio.html")
+        else: #se houver, busca ele
+            for i in dsPedido:
+                if i.status==0:
+                    pedido = Pedido.objects.get(pk=i.cod)
+                    
+    vProduto = Produto.objects.get(pk=cod_produto)
+    produto=Pedido_Produto.objects.filter(cod_pedido=pedido.cod, cod_produto=vProduto.cod)
+    for i in produto:
+        if i.cod_produto.cod==vProduto.cod:
+            vProduto.estoque=i.quantidade
+            if vProduto.estoque==quantidadeDeletar:
+                i.delete()
+            else:
+                i.quantidade-=quantidadeDeletar
+                i.save()
+    pedido.valor-=quantidadeDeletar*vProduto.valorUnitario
+    pedido.save()
+    
+    return redirect("/pedidos/"+str(mesa1)+"/carrinho/") #retorno pro cardapio
