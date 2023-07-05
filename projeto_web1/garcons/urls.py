b9bd74ff720610403c61a801d643bd9da573f6b6
João Vitor Nascimento <96423586+joaovitornso@users.noreply.github.com>
2023-07-04 21:42:08 -0300

    att-04/06/2023 -> Change in Garcom
    Mudanças no app de Garçons, criado a visualização das mesas, dos pedidos feitos por uma mesa e os produtos dentro de um pedido específico.

--- projeto_web1/garcons/urls.py
+++ projeto_web1/garcons/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("list_pedidos/", views.list_pedidos, name="list_pedidos"),
+    path("list_mesas/", views.list_mesas, name="list_mesas"),
     path("fazer_pedido/", views.fazer_pedido, name="fazer_pedido" ),
+    path("<int:mesa1>/list_pedidos/", views.list_pedidos, name="list_pedidos" ),
+    path("<int:mesa1>/list_pedido/", views.list_pedido, name="list_pedido" ),
+    
     
 ]
